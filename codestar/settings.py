7e198f06b63c601f34abc90f6153001a8dea291b
Guy Steele-Perkins <guysteeleperkins@hotmail.com>
2024-11-13 10:58:26 +0000

    Added database URL to DATABASES variable in setting.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "26031996Guysp"
+SECRET_KEY = os.environ.get("26031996Guysp")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("postgres://uidhmxkpawl:mHU9PfkE8pI2@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/stack_chess_speed_776212"))
 }
 
 # Password validation
