844929c5622c0d2fc8c8d66c9d85c784d2f9b66a
Guy Steele-Perkins <guysteeleperkins@hotmail.com>
2024-11-13 11:12:37 +0000

    creating database steps

--- codestar/settings.py
+++ codestar/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("postgres://uidhmxkpawl:mHU9PfkE8pI2@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/stack_chess_speed_776212"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 # Password validation
